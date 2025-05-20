afab5c0e76b645e27e152ae6b0c0a5560490e4ab
Jesus Victorio <jmvictoriom@caixabanktech.com>
2025-05-20 20:36:18 +0200

    prueba node

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.json({ "hola": "SI" });
 });
 
+app.get('/otroservicio', (req, res) => {
+  res.json({ "hola": "NO" });
+});
+
 // Iniciamos el servidor
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en puerto ${PORT}`);
